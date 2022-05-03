942158f3dbfe42681c791a31378b4061947c7274
Davit Khudaverdyan <davkhuda@gmail.com>
2022-05-02 23:26:22 -0400

    positioned footer on bottom of pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Outlet} from "react-router-dom"
 
 import "./App.css"
 import Sidebar from "./components/Sidebar/Sidebar"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Sidebar />
       <Outlet />
+      <Footer />
     </>
   );
 }
