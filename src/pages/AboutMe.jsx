5291e74e4dbd71656d4a911d50a7d9a0d9ceb813
Davit Khudaverdyan <davkhuda@gmail.com>
2022-05-07 09:33:38 -0400

    start about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,8 +7,7 @@ export default function AboutMe() {
       <div className="heading">
         <h1 className="title">About Me</h1>
       </div>
-      <nav className="about-me-nav">
-      </nav>
+      
       <section className="bio">
 
       </section>
