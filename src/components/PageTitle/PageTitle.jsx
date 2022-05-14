e468cdbc1e25a5b239bccea85253994483d6cd7d
Davit Khudaverdyan <davkhuda@gmail.com>
2022-05-14 13:32:32 -0400

    add mobile responsiveness to table, add about davo section header, madd margins to pagetitle

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import "./PageTitle.css"
 
 
-export default function PageHeader(props) {
+export default function PageTitle(props) {
   const {pageTitle} = props;
 
   return (
-    <div className="page-heading">
+    <div className="page-title">
       <h1>{pageTitle}</h1>
     </div>
   )
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './css/AboutMe.css'
 import PageTitle from '../components/PageTitle/PageTitle'
+
+
 export default function AboutMe() {
   return (
     <>
@@ -8,6 +10,9 @@ export default function AboutMe() {
       
       <div className="about-me-content">
         <section className="bio">
+          <h2 className="section-header"  id="about-me">
+            About Davo
+          </h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nemo dicta quia velit nesciunt molestiae. Esse quaerat, numquam doloribus quis, reiciendis harum quae repellat molestias quam qui nisi explicabo magni?</p>
           <p>Quae aliquid reprehenderit corrupti dolorem quisquam incidunt a non impedit? Rerum placeat earum, architecto nam non laboriosam veniam voluptatibus natus ipsa officiis, quia, praesentium quis veritatis pariatur. Nisi, nulla sint.</p>
           <p>Possimus aut velit perspiciatis repudiandae dolore doloremque iusto consequuntur consectetur similique laborum, asperiores quas, rem distinctio officiis provident repellendus cupiditate quaerat assumenda in placeat. Adipisci quibusdam unde laudantium id suscipit?</p>
@@ -17,18 +22,7 @@ export default function AboutMe() {
           <p>Possimus aut velit perspiciatis repudiandae dolore doloremque iusto consequuntur consectetur similique laborum, asperiores quas, rem distinctio officiis provident repellendus cupiditate quaerat assumenda in placeat. Adipisci quibusdam unde laudantium id suscipit?</p>
           <p>Laudantium dicta, in, cumque quos pariatur libero quod ullam accusantium praesentium neque explicabo corrupti vel fugit dolorum, nobis autem aut. Nemo vel placeat tenetur voluptates architecto sed rem corrupti neque.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nemo dicta quia velit nesciunt molestiae. Esse quaerat, numquam doloribus quis, reiciendis harum quae repellat molestias quam qui nisi explicabo magni?</p>
-          <p>Quae aliquid reprehenderit corrupti dolorem quisquam incidunt a non impedit? Rerum placeat earum, architecto nam non laboriosam veniam voluptatibus natus ipsa officiis, quia, praesentium quis veritatis pariatur. Nisi, nulla sint.</p>
-          <p>Possimus aut velit perspiciatis repudiandae dolore doloremque iusto consequuntur consectetur similique laborum, asperiores quas, rem distinctio officiis provident repellendus cupiditate quaerat assumenda in placeat. Adipisci quibusdam unde laudantium id suscipit?</p>
-          <p>Laudantium dicta, in, cumque quos pariatur libero quod ullam accusantium praesentium neque explicabo corrupti vel fugit dolorum, nobis autem aut. Nemo vel placeat tenetur voluptates architecto sed rem corrupti neque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nemo dicta quia velit nesciunt molestiae. Esse quaerat, numquam doloribus quis, reiciendis harum quae repellat molestias quam qui nisi explicabo magni?</p>
-          <p>Quae aliquid reprehenderit corrupti dolorem quisquam incidunt a non impedit? Rerum placeat earum, architecto nam non laboriosam veniam voluptatibus natus ipsa officiis, quia, praesentium quis veritatis pariatur. Nisi, nulla sint.</p>
-          <p>Possimus aut velit perspiciatis repudiandae dolore doloremque iusto consequuntur consectetur similique laborum, asperiores quas, rem distinctio officiis provident repellendus cupiditate quaerat assumenda in placeat. Adipisci quibusdam unde laudantium id suscipit?</p>
-          <p>Laudantium dicta, in, cumque quos pariatur libero quod ullam accusantium praesentium neque explicabo corrupti vel fugit dolorum, nobis autem aut. Nemo vel placeat tenetur voluptates architecto sed rem corrupti neque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nemo dicta quia velit nesciunt molestiae. Esse quaerat, numquam doloribus quis, reiciendis harum quae repellat molestias quam qui nisi explicabo magni?</p>
-          <p>Quae aliquid reprehenderit corrupti dolorem quisquam incidunt a non impedit? Rerum placeat earum, architecto nam non laboriosam veniam voluptatibus natus ipsa officiis, quia, praesentium quis veritatis pariatur. Nisi, nulla sint.</p>
-          <p>Possimus aut velit perspiciatis repudiandae dolore doloremque iusto consequuntur consectetur similique laborum, asperiores quas, rem distinctio officiis provident repellendus cupiditate quaerat assumenda in placeat. Adipisci quibusdam unde laudantium id suscipit?</p>
-          <p>Laudantium dicta, in, cumque quos pariatur libero quod ullam accusantium praesentium neque explicabo corrupti vel fugit dolorum, nobis autem aut. Nemo vel placeat tenetur voluptates architecto sed rem corrupti neque.</p>
-        
+
         </section>
         <section className="gear">
           <h2 className="section-header"  id="gear">
@@ -39,53 +33,64 @@ export default function AboutMe() {
             <p>This is all the gear currently used to take photos and videos:</p>
           </div>
 
-          <table className="bodies-table">
-            <caption>Camera Bodies</caption>
-            <thead>
-              <tr>
-                <th scope="col">Make</th>
-                <th scope="col">Body</th>
-                <th scope="col">Sensor Size</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Fujifilm</th>
-                <td>X-T4</td>
-                <td>APS-C</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="table-container">
+            <table className="bodies-table">
+              <caption>Camera Bodies</caption>
+              <thead>
+                <tr>
+                  <th scope="col">Make</th>
+                  <th scope="col">Body</th>
+                  <th scope="col">Sensor Size</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Fujifilm</th>
+                  <td>X-T4</td>
+                  <td>APS-C</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-          <table className="lens-table">
-            <caption>Lenses</caption>
-            <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Focal Length</th>
-                <th scope="col">Max Aperture Range</th>
-                <th scope="col">Filter Size</th>
-              </tr>
-            </thead>
-            
-            <tbody>
-              <tr>
-                <th scope="row">FUJIFILM XF 16-80mm f/4 R OIS WR</th>
-                <td>Zoom</td>
-                <td>16 - 80 mm</td>
-                <td>f/4</td>
-                <td>72 mm</td>
-              </tr>
-              <tr>
-                <th scope="row">FUJIFILM XF 70-300mm f/4-5.6 R LM OIS WR</th>
-                <td>Zoom</td>
-                <td>70 - 300 mm</td>
-                <td>f/4-5.6</td>
-                <td>67 mm</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="table-container">
+            <table className="lens-table">
+              <caption>Lenses</caption>
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Focal Length</th>
+                  <th scope="col">Max Aperture Range</th>
+                  <th scope="col">Filter Size</th>
+                </tr>
+              </thead>
+              
+              <tbody>
+                <tr>
+                  <th scope="row">FUJIFILM XF 16-80mm f/4 R OIS WR</th>
+                  <td>Zoom</td>
+                  <td>16 - 80 mm</td>
+                  <td>f/4</td>
+                  <td>72 mm</td>
+                </tr>
+                <tr>
+                  <th scope="row">FUJIFILM XF 70-300mm f/4-5.6 R LM OIS WR</th>
+                  <td>Zoom</td>
+                  <td>70 - 300 mm</td>
+                  <td>f/4-5.6</td>
+                  <td>67 mm</td>
+                </tr>
+                <tr>
+                  <th scope="row">Fujinon XF50mmF1.0 R WR</th>
+                  <td>Prime</td>
+                  <td>50 mm</td>
+                  <td>f/1</td>
+                  <td>77</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </section>
       </div>
     </>
